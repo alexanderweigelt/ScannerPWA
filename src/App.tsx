d9dec2a793f246c9114957f95fd6aab75756ab49
Alex <email@alexander-weigelt.de>
2026-02-19 08:43:43 +0100

    enhancement: Integrate jscanify for document processing; #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import {FC} from "react";
+import Script from 'next/script'
 import {CameraPreview} from "@/src/camera/CameraPreview";
 
 export const App:FC = () => {
   return (
-    <CameraPreview/>
-  )
+    <>
+      <Script src="https://docs.opencv.org/4.x/opencv.js" async={true} />
+      <CameraPreview/>
+    </>
+)
 }
